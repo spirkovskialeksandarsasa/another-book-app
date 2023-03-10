3b920cb6d7077c76e1af3baaa9abbf8a4b3f95ad
spirkovskialeksandarsasa <sasespirkovski@yahoo.com>
2023-03-10 15:04:44 +0100

    Bug Fixing and deploy on Github Pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="home">
       <div className="home__header">
-    <a href="/home">Available books</a>
+    <a href="/another-book-app/">Available books</a>
     </div>
 <div className="data-component">
       <DataComponent />
